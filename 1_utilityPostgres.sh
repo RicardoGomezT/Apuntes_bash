e79afe7e067b5d6e8413866fb12d86d1fef12c79
Richie Gomez <ricardo.gomezt@hotmail.es>
2021-07-23 16:29:54 -0500

    :sparkles: Develop: Actualización apuntes

---
+++ 1_utilityPostgres.sh
@@ -0,0 +1,4 @@
+# !/bin/bash
+# Programa para realizar algunas operaciones utilitarias de postgres
+echo "Hola mundo" #para imprimir
+
--- initial_test.sh
+++ initial_test.sh
@@ -5,3 +5,16 @@ vi apuntes.sh # antiguo
 
 #En el directorio de usuario cd $home configurar el archivo .vimrc
 #Para verlo como está oculto se usa el comando ls -la
+
+ls -l 1_utilityPostgres.sh # para ver los permisos y detalles
+sh 1_utilityPostgres.sh # para ejecutar el script, preferible tener permisos 
+chmod 764  1_utilityPostgres.sh # Permiso todo dueño, w y r usuario y r todo mundo
+chmod +x 1_utilityPostgres.sh # Darle permisos de ejecución a todos
+chmod o-x 1_utilityPostgres.sh # Quitarle solo a otros
+chmod g+x 1_fasdf.sh # darle solo a grupo
+
+#EJECUTAR
+
+sh 1_utilityPostgres.sh
+bash 1_utilityPostgres.sh
+./1_utilityPostgres.sh

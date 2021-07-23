757df1fafb7e5725c7eca371a906445d28accfc4
Richie Gomez <ricardo.gomezt@hotmail.es>
2021-07-23 15:58:39 -0500

    :sparkles: Develop: Archivo para configurar el editor vim .vimrc

--- initial_test.sh
+++ initial_test.sh
@@ -1,3 +1,7 @@
 vim apuntes.sh # actualizado
 vi apuntes.sh # antiguo
 # se escribe con i y se sale con esc :wq
+
+
+#En el directorio de usuario cd $home configurar el archivo .vimrc
+#Para verlo como está oculto se usa el comando ls -la
